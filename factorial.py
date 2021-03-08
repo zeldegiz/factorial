c4059e0e35742a90a4a281c71c20428f46b8cd36
zeldegiz <77746113+zeldegiz@users.noreply.github.com>
2021-03-08 13:38:27 +0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -4,6 +4,13 @@ def factorial(x,y):
         return x
     elif(m==1):
         return x*(x+1)
+    elif(m==2):
+        return x*(x+1)*(x+2)
+    elif(m==3):
+        return x*(x+1)*(x+2)*(x+3)
+    elif(m==4):
+        return x*(x+1)*(x+2)*(x+3)*(x+4)
     else:
-        k = int(m/2)
-        return factorial(x,x+k)*factorial(x+k+1,y)
+        k = int(m/5)
+        return factorial(x,x+k)*factorial(x+k+1,x+2*k)*factorial(x+2*k+1,x+3*k)*factorial(x+3*k+1,x+4*k)*factorial(x+4*k+1,y)
+
